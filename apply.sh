fab6fb0a6451100a1179c869f244f495227aae01
Rob Atkinson <rob@metalinkage.com.au>
2021-02-01 15:04:24 +1100

    Added update apply.sh style script

---
+++ apply.sh
@@ -0,0 +1,37 @@
+export VP_HOME = /var/www/vocprez-demeter
+export VP_THEME_HOME = /var/www/vocprez-demeter_skin
+export SPARQL_ENDPOINT = http://defs-dev.opengis.net:8080/rdf4j-server/repositories/demeter
+
+# copy all style content to VP
+echo "copying $VP_THEME_HOME/style content to $VP_HOME/vocprez/view/style"
+cp $VP_THEME_HOME/style/* $VP_HOME/vocprez/view/style
+
+# copy all templates to VP
+echo "copying $VP_THEME_HOME/templates content to $VP_HOME/vocprez/view/templates"
+cp $VP_THEME_HOME/templates/* $VP_HOME/vocprez/view/templates
+
+# alter app.py
+sed -n '/# ROUTE index/q;p' $VP_HOME/vocprez/app.py > $VP_THEME_HOME/app_temp.py
+cat $VP_THEME_HOME/app_additions.py >> $VP_THEME_HOME/app_temp.py
+sed -e '1,/# END ROUTE index/ d' $VP_HOME/vocprez/app.py >> $VP_THEME_HOME/app_temp.py
+mv $VP_THEME_HOME/app_temp.py $VP_HOME/vocprez/app.py
+
+echo "Config"
+echo "creating VocPrez config with $VP_THEME_HOME/config.py"
+echo "Alter config.py to include variables"
+sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
+if [ -z "$SPARQL_USERNAME" ]
+then
+      sed -i 's#$SPARQL_USERNAME#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_USERNAME#'\"$SPARQL_USERNAME\"'#' $VP_THEME_HOME/config_updated.py
+fi
+if [ -z "$SPARQL_PASSWORD" ]
+then
+      sed -i 's#$SPARQL_PASSWORD#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_PASSWORD#'\"$SPARQL_PASSWORD\"'#' $VP_THEME_HOME/config_updated.py
+fi
+mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
+
+echo "customisation done"
